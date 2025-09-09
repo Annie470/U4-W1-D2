be902e1f592182733dccdeb223e1ec444e35016a
Anna De Lise <anna_delise@libero.it>
2025-09-09 15:54:59 +0200

    add last exercise

--- src/esercizio4/Esercizio4.java
+++ src/esercizio4/Esercizio4.java
@@ -1,6 +1,15 @@
 package esercizio4;
 
+import java.util.Scanner;
+
 public class Esercizio4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci un numero: ");
+        int numero = Integer.parseInt(scanner.nextLine());
+        System.out.println("Inizio del conto alla rovescia");
+        for(int i = numero; i >= 0; i--) {
+            System.out.println(i);
+        }
     }
 }
